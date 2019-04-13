1b6f5ba1a80670b3067235330c806b285f096ab7
Lillo Azzoletti <lilloazzoletti@MBP-di-Lillo.station>
2019-04-13 20:17:29 +0200

    13/04 20.20

--- public/javascripts/principale.js
+++ public/javascripts/principale.js
@@ -1,10 +1,21 @@
 var app = angular.module("home", []);
 var user = "";
 var pass = "";
-app.controller("Principale", function () {
-    this.vedi = function ($http) {
-        console.log("User: " + this.user);
-        console.log("Password: " + this.pass);
+app.controller("Principale", function ($http) {
+    this.vedi = function () {
+        var data = {
+            "user": this.user,
+            "pass": this.pass
+        };
+        var config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        };
+        $http.post('http://localhost:3030/login', data, config).then(function (response) {
+            console.log("La risposta che arriva dal server: " + response.data);
+            //console.log(data);
+        });
     };
 
 });
--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,21 @@ var express = require('express');
 var router = express.Router();
 var fs = require('fs');
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  fs.readFile('index.html', function (err, data) {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write(data);
+router.post('/login', function (req, res, next) {
+  //fs.readFile('index.html', function (err, data) {
+  //res.writeHead(200, { 'Content-Type': 'text/html' });
+  var user = req.body.user;
+  var pass = req.body.pass;
+  if (user === 'Lillo' && pass === '1234') {
+    res.redirect('http://localhost:3030/home/index.html');
+  } else {
+    res.write("Mi dispiace ma non sei tu");
     res.end();
-  });
+  }
+});
+
+router.use(function (req, res, next) {
+  res.sendFile('notFound.html', { root: './public' })
 });
 
 module.exports = router;
