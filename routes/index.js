6ccf0e1ce2a378183803affbe8ebac7040d0a9dd
Lillo Azzoletti <lilloazzoletti@MBP-di-Lillo.station>
2019-04-13 17:12:28 +0200

    Avanzamento al 13 aprile

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
-
+var fs = require('fs');
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  fs.readFile('index.html', function (err, data) {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write(data);
+    res.end();
+  });
 });
 
 module.exports = router;
